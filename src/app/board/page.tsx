75edb7907477e4dd735a0f1485264339d6244cea
Alireza Kasirzare <alirezakasir@yahoo.com>
2025-06-05 22:04:55 +0330

    Feature : respnsive board page

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -4,7 +4,7 @@ const BoardPage = () => {
   return (
     <div className="mt-16 container">
       <h2 className="font-bold text-2xl">هیئت رئیسه</h2>
-      <div className="grid grid-cols-4 gap-10 mt-10">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-10 mt-10">
         <Person />
         <Person />
         <Person />
