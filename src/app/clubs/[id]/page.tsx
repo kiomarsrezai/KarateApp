0d648c5b604a195e204f8f2ad4a0e349826e8c3c
Alireza Kasirzare <alirezakasir@yahoo.com>
2025-06-05 22:00:22 +0330

    Feature : responsive single club page

--- src/app/clubs/[id]/page.tsx
+++ src/app/clubs/[id]/page.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import { MapPin, PhoneIcon } from "lucide-react";
 import Image from "next/image";
 import { AspectRatio } from "~/components/ui/aspect-ratio";
+import { useIsMobile } from "~/hooks/use-mobile";
 
 const SingleClubPage = () => {
+  const mobile = useIsMobile();
+
   return (
     <div>
       <div className="relative">
-        <AspectRatio ratio={35 / 9}>
+        <AspectRatio ratio={mobile ? 16 / 9 : 35 / 9}>
           <Image
             src={
               "https://images.unsplash.com/photo-1555597673-b21d5c935865?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
