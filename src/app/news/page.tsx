8be9e643c2e065dbfc259530bed1b4252dd6fc1a
Alireza Kasirzare <alirezakasir@yahoo.com>
2025-08-06 15:24:05 +0330

    Feature : add news page

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,9 +1,16 @@
-import { ComingSoon } from "~/components/seactions/ComingSoon";
+import { getNewsApi } from "~/components/features/news/api";
+import { NewsCard } from "~/components/features/news/NewsCard";
 
-const NewsPage = () => {
+const NewsPage = async () => {
+  const news = await getNewsApi();
+
   return (
-    <div className="mt-16 container">
-      <ComingSoon />
+    <div className="mt-10 lg:mt-28 pb-10  container">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {news.map((newsItem) => (
+          <NewsCard newsItem={newsItem} />
+        ))}
+      </div>
     </div>
   );
 };
